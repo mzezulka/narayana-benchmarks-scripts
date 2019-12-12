f2f3b6ba170a34db386bd01bdeccdba02b0704d3
Miloslav Žežulka <mzezulka@dhcp-10-40-5-80.brq.redhat.com>
2019-12-12 08:57:42 +0100

    Another changes enforced by introducing log4j2.

--- BenchmarkLogger.java
+++ BenchmarkLogger.java
@@ -2,9 +2,10 @@ package com.arjuna.ats.arjuna.logging;
 
 import java.util.UUID;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public final class BenchmarkLogger {
-    private static final Logger LOGGER = Logger.getLogger(BenchmarkLogger.class.getName());
+    private static final Logger LOGGER = LogManager.getLogger(BenchmarkLogger.class.getName());
     private static final String MSG = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed"
             + " do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim "
             + "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";

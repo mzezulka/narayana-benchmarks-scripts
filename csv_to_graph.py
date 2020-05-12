544439186144bdd3be4f5126301fbaf3ca18f8e4
Miloslav Žežulka <mzezulka@localhost.localdomain>
2020-05-12 15:57:40 +0200

    Sync nomenclature with thesis text (Narayana types).

--- csv_to_graph.py
+++ csv_to_graph.py
@@ -9,7 +9,7 @@ import math
 from plotly.subplots import make_subplots
 
 types = [ "file-logged", "noop", "jaeger", "vanilla", "tracing-off" ]
-rgbTriples = { "file-logged" : "254, 232, 200", "noop" : "254, 232, 200", "jaeger" : "227, 74, 51", "vanilla" : "253, 187, 132", "tracing-off" : "254, 232, 200"  }
+rgbTriples = { "file-logged" : "255,255,191", "noop" : "255,255,191", "jaeger" : "252,141,89", "vanilla" : "145,191,219", "tracing-off" : "255,255,191"  }
 
 def assignRgbTriple(t):
     if t in rgbTriples:
@@ -24,15 +24,27 @@ def typeFromFilename(filename):
     else:
         raise ValueError("got weird filename " + filename)
 
+def assignType(narayanaType):
+    if(narayanaType == "vanilla"):
+        return "T5"
+    elif(narayanaType == "tracing-off"):
+        return "T4"
+    elif(narayanaType == "noop"):
+        return "T3"
+    elif(narayanaType == "jaeger"):
+        return "T2"
+    elif(narayanaType == "file-logged"):
+        return "T1"
+
 if(len(sys.argv) < 2):
     raise ValueError("expected exactly one argument representing at least one input csv file")
 
-namesMap={}
 narayanaType=None
 lastSeenNarayana=typeFromFilename(sys.argv[1])
-highestColIndex=1
 dataTriples={}
 dataTriples[lastSeenNarayana]=[]
+
+
 
 for f in sorted(sys.argv[1:]):
     narayanaType = typeFromFilename(f)
@@ -46,55 +58,51 @@ for f in sorted(sys.argv[1:]):
         if(name != "twoPhaseCommit"):
             continue
         dataTriples[lastSeenNarayana].append((name, threads, score))
-        if name not in namesMap.keys(): 
-            namesMap[name] = highestColIndex
-            highestColIndex += 1
 
 fig = make_subplots(
-        rows=len(namesMap.keys()),
+        rows=3,
         shared_xaxes=True,
+        subplot_titles=("Number of Threads = 1", "Number of Threads = 2", "Number of Threads = 4"),
         cols=1,
-        subplot_titles=list(namesMap.keys()),
-        x_title="Number of Threads (-t JMH arg)",
+        x_title="Narayana Type",
         y_title="Operations Per Second")
-dotSize = 10
 for narayanaType in dataTriples.keys():
     oneType = dataTriples[narayanaType]
     if(oneType is None or len(oneType) == 0):
         print("Empty result set for '" + narayanaType + "', skipping...")
         continue
     (name, threads, score) = oneType[0]
+    narayanaTypeMethod=assignType(narayanaType)
     fig.add_trace(go.Bar(
-                     x=[threads],
+                     x=[narayanaTypeMethod],
                      y=[score],
-                     name=narayanaType,
-                     legendgroup=narayanaType,
-                     # marker_size=dotSize,
+                     y0=0,
+                     name=str(threads),
                      marker_color=assignRgbTriple(narayanaType)),
-                     row=namesMap[name],
+                     row=int(math.log(threads,2)) + 1,
                      col=1)
     for (name, threads, score) in oneType[1:]:
        fig.add_trace(go.Bar(
-                        showlegend=False,
-                        x=[threads],
+                        x=[narayanaTypeMethod],
                         y=[score],
-                        name=narayanaType,
-                        legendgroup=narayanaType,
-                        # marker_size=dotSize,
+                        y0=0,
+                        name=str(threads),
+                        legendgroup=narayanaTypeMethod,
                         marker_color=assignRgbTriple(narayanaType)),
-                        row=namesMap[name],
+                        row=int(math.log(threads,2)) + 1,
                         col=1)
 
 fig.update_xaxes(type="category")
 fig.update_yaxes(type="log")
+fig.update_yaxes(ticks="outside", tickwidth=2, tickcolor='crimson', ticklen=10, col=1)
 fig.update_layout(
     barmode='group',
-    width=800,
+    width=700,
     font=dict(
         family="Arial",
         size=12,
         color="#1f1f1f"
-    )
+    ),
+    showlegend=False
 )
 fig.show()
-
